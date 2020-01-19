de25328dca8d07f2e9c47548e5992ccaeca573db
tshinowpub <wayfarermusic0130@gmail.com>
2020-01-20 05:08:05 +0900

    add 簡単なJsonResponseを返せるようにしてみた

---
+++ app/com/github/tshinow/orbit/application/controllers/StreamController.scala
@@ -0,0 +1,24 @@
+package com.github.tshinow.orbit.application.controllers
+
+import javax.inject._
+import play.api.libs.json.Json
+import play.api.mvc._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class StreamController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    Ok(Json.toJson(Seq("test1","test2","test3")))
+  }
+}
