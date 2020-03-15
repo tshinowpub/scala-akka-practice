273b8ea8ea3555b6821b46c3814efbf76fa86515
tshinowpub <tshinow@gmail.com>
2020-03-16 04:15:16 +0900

    remove 利用していない不要なクラスを削除した

--- app/com/github/tshinow/orbit/domain/value/StreamingService.scala
+++
@@ -1,6 +0,0 @@
-package com.github.tshinow.orbit.domain.value
-
-sealed abstract class StreamingService(val value: String)
-
-case object Youtube extends StreamingService("1")
-case object Twitch extends StreamingService("2")
