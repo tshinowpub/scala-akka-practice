7bbe17ac5b7f9c22e6e131b5492805d0342bfd85
tshinowpub <tshinow@gmail.com>
2020-06-01 04:39:36 +0900

    add 送信先のURLをさサーバーから渡した変数で定義できるようにした

--- app/com/github/tshinow/orbit/application/controllers/auth/AuthenticationController.scala
+++ app/com/github/tshinow/orbit/application/controllers/auth/AuthenticationController.scala
@@ -2,10 +2,13 @@ package com.github.tshinow.orbit.application.controllers.auth
 
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Request}
+import play.api.Configuration
 
 @Singleton
-class AuthenticationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class AuthenticationController @Inject()(val controllerComponents: ControllerComponents, val configuration: Configuration) extends BaseController {
   def signup(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.signup())
+    val baseUrl = configuration.get[String]("baseUrl")
+
+    Ok(views.html.signup(baseUrl))
   }
 }
